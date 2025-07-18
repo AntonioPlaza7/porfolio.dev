40aec17c87efe8e2a3b43a1b1345b52bbcac3ed2
AntonioPlaza7 <antoniojoseplacu@gmail.com>
2025-07-18 19:07:36 +0200

    Mi porfolio en Astro

---
+++ tailwind.config.mjs
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: 'class',
+  content: [
+    './src/**/*.{astro,js,ts,jsx,tsx}',
+    './public/**/*.html'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
